bf8f29bb17ed806897de2b2f4a7145d93f037a96
yuankun.zhang <fmcocoa@gmail.com>
2016-11-18 15:13:35 +0800

    copy_to_unity_sdk.sh

---
+++ Scripts/copy_to_unity_sdk.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# go to project root.
+if [[ $(pwd) == *Scripts ]]; then
+    cd ..
+fi
+
+ver=$(grep "VERSION = " FunPlusSDK/src/main/java/com/funplus/sdk/FunPlusSDK.java | sed "s/public static final String VERSION = //g" | tr -d ' ;"')
+f=funplus-android-sdk-$ver.jar
+
+src=Release/funplus-android-sdk-$ver/$f
+target_dir=../../unity/sdk-core/Assets/FunPlusSDK/Plugins/Android
+
+if [ -f $target_dir/funplus-android-sdk* ]; then
+    rm $target_dir/funplus-android-sdk*
+fi
+
+dst=$target_dir/$f
+
+cp $src $dst
+
+echo Copied $src to $dst
